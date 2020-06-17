bd85b2fa036f52c913feaee23b76f20be6f65e71
Robert Nyström <golgafrincham@gmail.com>
2020-06-17 16:36:45 +0200

    decorator for avg execution time

--- decorators.py
+++ decorators.py
@@ -19,3 +19,31 @@ def timed(fn):
 
         return result
     return inner
+
+
+def timed_avg(fn):
+    @wraps(fn)
+    def inner(*args, **kwargs):
+        elapsed_total = 0
+        elapsed_count = 0
+
+        for i in range(10):
+            start = perf_counter()
+            result = fn(*args, **kwargs)
+            end = perf_counter()
+            elapsed = end - start
+            elapsed_total += elapsed
+            elapsed_count += 1
+
+        args_ = [str(a) for a in args]
+        kwargs_ = [f"{k}={v}" for (k, v) in kwargs.items()]
+        all_args = args_ + kwargs_
+        args_str = ", ".join(all_args)
+
+        elapsed_avg = elapsed_total / elapsed_count
+        print(f"{fn.__name__}({args_str}) took on average",
+              f"{elapsed_avg:.6f}s to run.",
+              f"Averaged over {elapsed_count} times")
+
+        return result
+    return inner
