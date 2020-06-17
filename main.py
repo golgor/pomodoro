ed042792eb7692746e3af99827e4bb15359bbac2
Robert Nyström <golgafrincham@gmail.com>
2020-06-17 16:08:34 +0200

    decorator for timing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from alarms import create_alarm_time, is_alarm
 from time import sleep
 from playsound import playsound
-
+from decorators import timed
 
 if __name__ == "__main__":
     print("Do you want to start the Pomodoro timer (y/n)?")
