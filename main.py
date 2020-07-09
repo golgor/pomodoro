ef073f1ae70d61c2f8cf874f245598e8d25271da
Robert Nyström <golgafrincham@gmail.com>
2020-07-09 16:21:11 +0200

    changed project structure

--- main.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-from playsound import playsound
-from pomodoro.decorators import timed, timed_avg, logged
-from pomodoro.alarms import create_alarm_time, is_alarm
-
-
-if __name__ == "__main__":
-    print("Running Main.py!")
